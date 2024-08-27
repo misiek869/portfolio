b3023cfda017ddddc9d594535e5a8ef3abe35e83
Michal Gawik <gawlik.zator@o2.pl>
2024-08-27 14:13:57 +0200

    add project images and description

--- src/app/page/portfolio-design/index.tsx
+++ src/app/page/portfolio-design/index.tsx
@@ -35,6 +35,63 @@ const index = ({ data, id, dataArray }: PortfolioDesignProps) => {
 					</div>
 				</div>
 			</div>
+			<div
+				className='grid  grid-cols-12  relative space-x-0 lg:space-x-8'
+				style={{
+					background:
+						'linear-gradient(90deg, rgba(238, 247, 251, 1) 58%, rgba(255, 255, 255, 1) 52%',
+				}}>
+				<div className='col-span-12  lg:col-span-8 mb-20 bg-scroll lg:px-0 sm:px-20'>
+					{data?.images.map(item => (
+						<div
+							key={item}
+							className='flex justify-center lg:justify-end  items-center'>
+							<Image
+								src={item}
+								alt='portfolio photos'
+								height={100}
+								width={800}
+								className='mt-20 rounded-md'
+							/>
+						</div>
+					))}
+				</div>
+				<div className='col-span-12  lg:col-span-4 lg:px-0 sm:px-20'>
+					<div className='mt-10 sm:mt-24 w-full lg:max-w-[300px] lg:px-1  px-3 sticky top-36 pb-14'>
+						<h1 className='text-3xl  mb-4 text-[#48AFDE]'>{data?.name}</h1>
+						<p className='text-[14px] font-sans mb-4 text-[#223740]'>
+							{data?.des}
+						</p>
+						<p id='highlight' className='my-2  text-dark text-[20px] font-sans'>
+							Project Descriptions
+						</p>
+						<p className='text-[14px] font-sans mb-4 text-[#223740]'>
+							{data?.des1}
+						</p>
+
+						<div className='flex flex-wrap'>
+							<h1 className='mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]'>
+								UI/US Design
+							</h1>
+							<h1 className='mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]'>
+								Next.js
+							</h1>
+							<h1 className='mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]'>
+								React.js
+							</h1>
+							<h1 className='mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]'>
+								Node.js
+							</h1>
+							<h1 className='mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]'>
+								Express
+							</h1>
+							<h1 className='mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]'>
+								UI/US Design
+							</h1>
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
