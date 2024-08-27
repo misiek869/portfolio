7a73dc907c6fdd8ad2cd0672dd593280a5f8bec0
Michal Gawik <gawlik.zator@o2.pl>
2024-08-27 08:40:14 +0200

    add w-full

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -36,7 +36,7 @@ const Header = () => {
 			<header
 				className={`${
 					isScrolled ? 'headerShow' : ''
-				} fixed top-0 z-50 transition-all duration-500`}
+				} w-full fixed top-0 z-50 transition-all duration-500`}
 				style={{
 					backgroundColor: isScrolled ? '#fff' : 'transparent',
 					boxShadow: isScrolled ? '#48AFDE -10px 25px 50px 10px' : '',
