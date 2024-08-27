fa3b716bf4d370a0e79b106eea0a0080841ac205
Michal Gawik <gawlik.zator@o2.pl>
2024-08-27 05:37:17 +0200

    set useEffect and useState

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+'use client'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
+	const [selectedIndexOne, setSelectedIndexOne] = useState(0)
+	const [isOpen, setIsOpen] = useState(false)
+	const [isScrolled, setIsScrolled] = useState(false)
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+			setIsScrolled(scrollTop > 0)
+		}
+		window.addEventListener('scroll', handleScroll)
+		return () => {
+			window.removeEventListener('scroll', handleScroll)
+		}
+	}, [])
+
+	console.log(isScrolled)
+
 	return <div></div>
 }
 
