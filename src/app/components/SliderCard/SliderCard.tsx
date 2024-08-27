ac3eb3754a3a35abcf2f49b503b7d7b5012a7f6a
Michal Gawik <gawlik.zator@o2.pl>
2024-08-27 11:17:17 +0200

    create card

--- src/app/components/SliderCard/SliderCard.tsx
+++ src/app/components/SliderCard/SliderCard.tsx
@@ -7,7 +7,15 @@ const SliderCard = ({
 	item: DataArrayType
 	index: number
 }) => {
-	return <></>
+	return (
+		<>
+			<div className='h-[280px] lg:h-[450px] md:h-[400px] sm:h-[200px] lg:w-[650px] md:w-[450px]  sma:w-[300px] w-[300px] relative m-auto'>
+				<div className='h-full w-full group cursor-all-scroll z-50  relative py-4 md:py-4'>
+					<img src={item.images[0]} alt={item.name} />
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default SliderCard
