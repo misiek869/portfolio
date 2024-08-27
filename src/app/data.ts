91c33c38dd8de00570f7688e41b04493585148d3
Michal Gawik <gawlik.zator@o2.pl>
2024-08-27 11:24:11 +0200

    change images

--- src/app/data.ts
+++ src/app/data.ts
@@ -109,7 +109,7 @@ export const DataArray: DataArrayType[] = [
 		name: 'Ultimate ReactJs portfolio',
 		des: 'I have made this project in 2022  to show thousands of students all over the world  how to make Reactjs portfolio.',
 		des1: 'This project was made using, Ractjs, Nextjs, Expressjs, Nodejs, Rxjs Bootsrap and many more!',
-		images: ['/reactportfolio.png', '/2.webp', '/3.webp', '/4.webp'],
+		images: ['/portfolio-image.jpg'],
 	},
 	{
 		title: 'Management Consulting Company Web Design',
@@ -117,7 +117,7 @@ export const DataArray: DataArrayType[] = [
 		des: 'We buit this project for edubaba  Management Consultants.',
 		des1: '',
 
-		images: ['/hotel01.png'],
+		images: ['/portfolio-image.jpg'],
 	},
 	{
 		title: 'Food Delivery Company WebSite',
@@ -125,7 +125,7 @@ export const DataArray: DataArrayType[] = [
 		des: 'We did  this project for Yum Foods Located in USA.',
 		des1: "This website's graphic design is fantastic. Animated sliders are painstakingly built and created in WordPress with Revolution Slider. All of the product photos are photo manipulations created in Adobe Photoshop.",
 
-		images: ['/yumfood.jpg', '/yumfood2.png'],
+		images: ['/portfolio-image.jpg', '/portfolio-image.jpg'],
 	},
 	{
 		title: 'Real Estate and Property Management Project',
@@ -133,7 +133,7 @@ export const DataArray: DataArrayType[] = [
 		des: 'We built this project for inans group LTD',
 		des1: 'Aside from its gorgeous design, this website is backed by an outstanding plugin that allows owners and agencies to simply display different properties in the neighbourhood and create an online showcase in an appealing manner for home buyers and tenants..',
 
-		images: ['/property1.png'],
+		images: ['/portfolio-image.jpg'],
 	},
 	{
 		title: 'Crypto Trading Website',
@@ -141,6 +141,10 @@ export const DataArray: DataArrayType[] = [
 		des: 'We buit this website for BigMoney group',
 		des1: 'This project was built to enable clients all over the world to buy and sell their crypto.',
 
-		images: ['/crypto5.png', '/crypto3.png', '/crypto1.png'],
+		images: [
+			'/portfolio-image.jpg',
+			'/portfolio-image.jpg',
+			'/portfolio-image.jpg',
+		],
 	},
 ]
