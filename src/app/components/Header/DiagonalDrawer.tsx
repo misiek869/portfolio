c4ac8246fb2b3997dab172d10dae8776b5c20bd5
Michal Gawik <gawlik.zator@o2.pl>
2024-08-27 08:49:46 +0200

    add svgs

--- src/app/components/Header/DiagonalDrawer.tsx
+++ src/app/components/Header/DiagonalDrawer.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import { useRouter, usePathname } from 'next/navigation'
 
 type diagonalDrawerProps = {
 	isOpen: boolean
@@ -13,7 +14,51 @@ const DiagonalDrawer = ({
 	selectedIndexOne,
 	setSelectedIndexOne,
 }: diagonalDrawerProps) => {
-	return <div></div>
+	const route = useRouter()
+	const pathName = usePathname()
+
+	return (
+		<>
+			<div className='relative'>
+				<div
+					className={`z-50 ${
+						pathName === '/' ? 'top-[0px]' : 'top-[0px]'
+					} cursor-pointer fixed w-14 h-14 lg:w-24 lg:h-24 bg-[#48AFDE] top-0 flex justify-center items-center rounded-br-3xl `}>
+					<div className='relative flex justify-center items-center w-7 h-7 lg:w-10 lg:h-10 '>
+						{isOpen ? (
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								fill='none'
+								viewBox='0 0 24 24'
+								strokeWidth={1.5}
+								stroke='currentColor'
+								class='w-12 h-12 text-white cursor-pointer  '>
+								<path
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									d='M6 18L18 6M6 6l12 12'
+								/>
+							</svg>
+						) : (
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								fill='none'
+								viewBox='0 0 24 24'
+								stroke-width='1.5'
+								stroke='currentColor'
+								class='w-12 h-12 text-white'>
+								<path
+									stroke-linecap='round'
+									stroke-linejoin='round'
+									d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12'
+								/>
+							</svg>
+						)}
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default DiagonalDrawer
