0de2e76cc9b3fe2cdeb2e7c80e75e26dbe0ab536
Michal Gawik <gawlik.zator@o2.pl>
2024-08-28 12:31:14 +0200

    install headlessui

--- src/app/components/Footer/Accordion.tsx
+++ src/app/components/Footer/Accordion.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
 
 const Accordion = ({
 	title,
@@ -7,7 +8,55 @@ const Accordion = ({
 	title: string
 	content: string[]
 }) => {
-	return <div></div>
+	const [isOpen, setIsoOpen] = useState<boolean>(false)
+
+	const toggleAccordion = () => {
+		setIsoOpen(!isOpen)
+	}
+
+	return (
+		<div className='border-b border-[#232323] mb-4'>
+			<div
+				className='flex justify-between items-center p-2 sm:p-4 w-full bg-[#232323]'
+				onClick={toggleAccordion}>
+				<h2 className='text-2xl text-white font-semibold'>{title}</h2>
+				<div className='bg-[#232323] h-8 w-8 flex justify-center items-center rounded-full'>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						fill='none'
+						viewBox='0 0 24 24'
+						strokeWidth={1.5}
+						stroke='currentColor'
+						className={`w-6 h-6 text-white ${isOpen ? '' : '-rotate-90'}`}>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M19.5 8.25l-7.5 7.5-7.5-7.5'
+						/>
+					</svg>
+				</div>
+			</div>
+			{/* 
+			<Transition
+				show={isOpen}
+				enter='transition duration-200 ease-in'
+				enterFrom='opacity-0'
+				enterTo='opacity-100'
+				leave='transition duration-200 ease-out'
+				leaveFrom='opacity-100'
+				leaveTo='opacity-0'>
+				{ref => (
+					<div ref={ref} className='p-4 bg-[#232323]'>
+						{content.map(item => (
+							<p key={item} className='py-1 text-gray-400 font-sans'>
+								{item}
+							</p>
+						))}
+					</div>
+				)}
+			</Transition> */}
+		</div>
+	)
 }
 
 export default Accordion
