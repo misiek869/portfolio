18b67c940722683b2b9e20a82ff5fecdb3093fb8
Michal Gawik <gawlik.zator@o2.pl>
2024-08-28 05:28:55 +0200

    create h1

--- src/app/components/AboutMe/About.tsx
+++ src/app/components/AboutMe/About.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 const About = () => {
-	return <div></div>
+	return (
+		<>
+			<div
+				id='about-me-component'
+				className='bg-[#E0F3FD] pt-[250px] mt-4  pb-[100px] lg:pb-[600px] relative lg:h-[800px]'>
+				<div className='container m-auto'>
+					<h1 className='text-[240px] w-[80%] overflow-hidden absolute lg:left-40 md:left-30 top-[-50px]  text-[#EAF7FC]'>
+						About Me
+					</h1>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default About
