d2750f3f8ad6b63559c4921d37d0d1196adcbe77
Michal Gawik <gawlik.zator@o2.pl>
2024-08-28 11:20:30 +0200

    render footer

--- src/app/components/AboutMe/About.tsx
+++ src/app/components/AboutMe/About.tsx
@@ -163,13 +163,13 @@ const About = () => {
 										xmlns='http://www.w3.org/2000/svg'
 										fill='none'
 										viewBox='0 0 24 24'
-										stroke-width='2'
+										strokeWidth='2'
 										stroke='currentColor'
 										aria-hidden='true'
 										className='w-6 h-6 text-white'>
 										<path
-											stroke-linecap='round'
-											stroke-linejoin='round'
+											strokeLinecap='round'
+											strokeLinejoin='round'
 											d='M15 19l-7-7 7-7'></path>
 									</svg>
 								</a>
@@ -180,13 +180,13 @@ const About = () => {
 										xmlns='http://www.w3.org/2000/svg'
 										fill='none'
 										viewBox='0 0 24 24'
-										stroke-width='2'
+										strokeWidth='2'
 										stroke='currentColor'
 										aria-hidden='true'
 										className='w-6 h-6 text-white'>
 										<path
-											stroke-linecap='round'
-											stroke-linejoin='round'
+											strokeLinecap='round'
+											strokeLinejoin='round'
 											d='M9 5l7 7-7 7'></path>
 									</svg>
 								</a>
