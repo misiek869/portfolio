82365e2ed4027965a358d18a047a4294cfb21063
Michal Gawik <gawlik.zator@o2.pl>
2024-08-28 14:34:43 +0200

    create component

--- src/app/components/CallToAction/Cta.tsx
+++ src/app/components/CallToAction/Cta.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+'use client'
+import { useEffect, useState } from 'react'
 
 const Cta = () => {
-	return <div></div>
+	const [isScrolled, setIsScrolled] = useState(false)
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+			setIsScrolled(scrollTop > 0)
+		}
+		window.addEventListener('scroll', handleScroll)
+		return () => {
+			window.removeEventListener('scroll', handleScroll)
+		}
+	}, [])
+
+	return (
+		<div>
+			{isScrolled ? (
+				<div className='bg-red-500 p-4 w-fit rounded-md fixed bottom-8 right-8 cursor-pointer z-50'>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						fill='none'
+						viewBox='0 0 24 24'
+						strokeWidth={1.5}
+						stroke='currentColor'
+						className='w-6 h-6 text-white'>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M4.5 15.75l7.5-7.5 7.5 7.5'
+						/>
+					</svg>
+				</div>
+			) : (
+				''
+			)}
+		</div>
+	)
 }
 
 export default Cta
