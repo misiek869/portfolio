009d72b66aedb136becc4919f7121760ee4bcb6f
Michal Gawik <gawlik.zator@o2.pl>
2024-09-02 09:57:01 +0200

    change styling

--- src/app/components/AboutMe/About.tsx
+++ src/app/components/AboutMe/About.tsx
@@ -49,7 +49,7 @@ const About = () => {
 					<h1 className='text-[240px] w-[80%] overflow-hidden absolute lg:left-40 md:left-30 top-[-50px]  text-[#EAF7FC]'>
 						About Me
 					</h1>
-					<h1 className='relative font-recoletaBlack text-5xl  text-[#48AFDE] mb-5 -mt-40  md:px-24 px-5'>
+					<h1 className='relative text-5xl  text-[#48AFDE] mb-5 -mt-40  md:px-24 px-5'>
 						About My Self
 					</h1>
 					<h4 className='relative w-full font-[300] md:w-3/4 lg:w-2/3 xl:w-1/2 font-recoleta text-[#223740] text-2xl mb-10 px-5 md:px-24'>
--- src/app/components/ContactMe/ContactMe.tsx
+++ src/app/components/ContactMe/ContactMe.tsx
@@ -96,14 +96,14 @@ const ContactMe = () => {
 				id='contactme'>
 				<div className='flex flex-col items-center justify-center bg-[url("/offices.jpg")] bg-cover bg-center w-full h-[400px]'>
 					<div className='flex flex-col items-center justify-center  w-full h-full bg-[#223740]/70 backdrop-brightness-50'>
-						<div className='text-center bg-[#48AFDE] py-[5px] px-[10px] tracking-wide uppercase font-semibold text-[20px] text-white rounded-lg'>
+						{/* <div className='text-center bg-[#48AFDE] py-[5px] px-[10px] tracking-wide uppercase font-semibold text-[20px] text-white rounded-lg'>
 							<a href='#contact' className='cursor-pointer'>
 								Contact Form
 							</a>
-						</div>
+						</div> */}
 						<div className='mt-[10px]'>
-							<h1 className='recoletaBold text-5xl text-[#48AFDE] p-2 text-center tracking-wide'>
-								Hire Me
+							<h1 className='text-5xl text-orange p-2 mb-5 text-center tracking-wide'>
+								Contact Me
 							</h1>
 						</div>
 						<div className='flex justify-center text-center mt-[10px] md:w-[700px] '>
@@ -120,15 +120,15 @@ const ContactMe = () => {
 					<div className='sm:flex gap-[20px] mt-[50px] max-w-[1105px] w-full h-full'>
 						<div className='flex flex-col flex-1 xl:flex-row gap-[20px]'>
 							<div
-								className='flex flex-col py-[35px] px-[45px] rounded-lg shadow-lg cursor-pointer transition-all transform duration-300 hover:bg-[#223740] hover:text-white hover:-translate-y-4 bg-white'
-								style={{ boxShadow: '#48AFDE -1px 1px 10px 0px' }}>
+								className='flex flex-col py-[35px] px-[45px] rounded-lg shadow-lg cursor-pointer transition-all transform duration-300 hover:bg-[gray] hover:text-white hover:-translate-y-4 bg-white'
+								style={{ boxShadow: 'orange -1px 1px 10px 0px' }}>
 								<div className='flex items-center gap-[15px]'>
 									<div>
-										<FaLocationDot size={40} color='#48AFDE' />
+										<FaLocationDot size={40} color='orange' />
 									</div>
 
 									<div className='flex flex-col'>
-										<div className='text-[#48AFDE] text-[14px]'>
+										<div className='text-orange text-[14px]'>
 											<p>Location...</p>
 										</div>
 										<div className='font-semibold text-[20px] tracking-wide'>
