87f2cfba0587c53c1cf6472c82024b747f7a2a5f
Michal Gawik <gawlik.zator@o2.pl>
2024-09-03 09:31:44 +0200

    change styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,16 @@ export default function HomePage() {
 	return (
 		<main className='min-h-screen relative'>
 			<Home />
-			<div
-				id='portfolio'
-				className='mt-0 pt-5 -mb-40'
-				style={{
-					backgroundImage:
-						'linear-gradient(-62deg, #EEF7FB 0 50%,  white 0% 100%)',
-					width: '100%',
-				}}>
+			<div id='portfolio' className='mt-0 pt-5 -mb-40'>
 				<div className='container m-auto'>
 					<p
-						className='text-[300px]  text-[#F7FBFD] md:pl-[50px] px-5 max-w-[750px] w-[100%] overflow-hidden'
-						style={{ transform: 'translate(0px,-20px)' }}>
+						className='text-[240px] text-opacity-5 text-black text-center mt-1 w-full overflow-hidden'
+						// style={{ transform: 'translate(0px,-20px)' }}
+					>
 						portfolio
 					</p>
 					<div className='' style={{ transform: 'translate(0px, -250px)' }}>
-						<p className='text-[#48AFDE]  md:pl-[80px] px-5 font-extrabold text-5xl'>
+						<p className='text-orange capitalize  md:pl-[80px] px-5 font-extrabold text-5xl'>
 							recent work
 						</p>
 						<p className='max-w-2xl md:pl-[80px] px-5 font-[300] text-[16px] text-[#47626D] leading-8 mt-5'>
