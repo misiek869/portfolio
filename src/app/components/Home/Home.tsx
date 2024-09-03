e7a26b3ab721f75de4adc3fb535b2b58bfb582f5
Michal Gawik <gawlik.zator@o2.pl>
2024-09-03 09:08:09 +0200

    change bg color

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -102,10 +102,10 @@ const Home = () => {
 				}}>
 				<div className='container m-auto'>
 					<div className='grid grid-cols-12 text-white'>
-						<div className='bg-white flex flex-col justify-center col-span-12 md:col-span-5 md:bg-transparent'>
+						<div className=' flex flex-col justify-center col-span-12 md:col-span-5 md:bg-transparent'>
 							<div className='container m-auto'>
 								<div className='lg:pl-24 sm:pl-10 pl-0 py-20 md:py-0 md:text-start text-center'>
-									<p className='text-3xl md:text-base lg:text-2xl '>Hello</p>
+									<p className='text-4xl md:text-base lg:text-3xl '>Hello</p>
 									<h1 className='text-5xl md:text-5xl lg:text-7xl xl:text-7xl mt-5 md:mt-3'>
 										I'm Michael
 									</h1>
